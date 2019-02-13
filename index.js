853925db5e2411340195b8d4c2bdfdec1320dd69
yintii <kheart@keleheart.com>
2019-02-13 12:19:02 -0600

    Made some tweaks and changes for the geocoding to work, but not working yet. Having an issue with the api service, I found I need to enable geocoding in the google back end. Not getting an error that triggers the not successful msg in the map.js for geocoding, but it is not changing the location on the map to the new location either.

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ app.use('/static', express.static(path.join(__dirname, "public")))
 app.set('view engine', 'pug')
 
 app.get('/', (req,res) => {
-    res.render('index', { title: 'Hello' })
+    res.render('index')
 })
+
+app.post('/', (req,res) => {
+    res.render('index')
+})
+
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`))
--- public/map.js
+++ public/map.js
@@ -1,6 +1,33 @@
+
 function initMap(){
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 30.2673, lng: -97.7431},
         zoom: 12
     });
+    var geocoder = new google.maps.Geocoder();
+    var infoWindow = new google.maps.InfoWindow;
+    var submitButton = document.getElementById("query");
+
+    submitButton.addEventListener("click", function(){
+        geocodeAddress(geocoder, map)
+    });
+
+
+    function geocodeAddress(geocoder, resultsMap){
+        var address = document.getElementById("inputbox").value;
+        geocoder.geocode({'address': address}, function(results, status){
+            if(status === 'OK'){
+                resultsMap.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                    map: resultsMap,
+                    position: results[0].geometry.location
+                });
+            }else{
+                alert('Geocode was not successful because ' + status);
+            }
+        });
+    }
+
+
 }
+
